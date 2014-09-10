bc922f1d289c29275f2e6583a448ddc68b534e3c
Robert Chang <cht8687@gmail.com>
2014-09-10 22:32:38 +1000

    feat(history): add arrows icon

--- www/js/services.js
+++ www/js/services.js
@@ -4,51 +4,58 @@ angular.module('starter.services', [])
 /**
  * A simple example service that returns some data.
  */
-.factory('friends', function() {
+.factory('friends', function () {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var friends = [
-    { id: 0, name: 'Scruff McGruff' },
-    { id: 1, name: 'G.I. Joe' },
-    { id: 2, name: 'Miss Frizzle' },
-    { id: 3, name: 'Ash Ketchum' }
-  ];
+  var friends = [{
+    id: 0,
+    name: 'Haotian'
+  }, {
+    id: 1,
+    name: 'Mao'
+  }, {
+    id: 2,
+    name: 'Mark Zurkburg'
+  }, {
+    id: 3,
+    name: 'Bill Gates'
+  }];
 
   // Public API
   return {
-    all: function() {
+    all: function () {
       return friends;
     },
-    get: function(friendId) {
+    get: function (friendId) {
       // Simple index lookup
       return friends[friendId];
     }
   };
 })
 
-.factory('account', function($http){
+.factory('account', function ($http) {
   var baseUrl = 'https://www.opal.com.au/';
-  var init = function(cb) {
+  var init = function (cb) {
     $http.get(baseUrl)
-      .success(function(data, status, header) {
+      .success(function (data, status, header) {
         cb(data, status, header);
       });
   };
 
-  var login = function(username, password, cb) {
+  var login = function (username, password, cb) {
     $http.post(baseUrl + 'login/registeredUserUsernameAndPasswordLogin' +
       '?h_username=' + username +
       '&h_password=' + password
     )
-      .success(function(data, status, header) {
+      .success(function (data, status, header) {
         cb(data, status, header);
       });
   };
 
-  var logout = function(cb) {
+  var logout = function (cb) {
     $http.post('')
-      .success(function(data, status, header) {
+      .success(function (data, status, header) {
         cb(data, status, header);
       });
   };
