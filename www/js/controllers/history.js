5b9eaa390311a4839a102072453dfcdf9f05d746
Robert Chang <cht8687@gmail.com>
2014-09-24 00:03:45 +1000

    feat(angular-charts): add in example # got a undefined function exception

--- www/js/controllers/history.js
+++ www/js/controllers/history.js
@@ -2,4 +2,40 @@
 angular.module('starter.controllers')
 	.controller('HistoryCtrl', function ($scope, card) {
 		$scope.cards = card.all();
+		$scope.config = {
+			title: '',
+			tooltips: true,
+			labels: false,
+			mouseover: function () {},
+			mouseout: function () {},
+			click: function () {},
+			legend: {
+				display: true,
+				position: 'left',
+				htmlEnabled: false
+			},
+			colors: [],
+			innerRadius: 0,
+			lineLegend: 'lineEnd',
+			lineCurveType: 'cardinal',
+			isAnimate: true,
+			yAxisTrickFormat: 's'
+		};
+
+		$scope.data = {
+			"series": [
+				"Sales",
+				"Income",
+				"Expense"
+			],
+			"data": [{
+				"x": "Computers",
+				"y": [
+					54,
+					0,
+					879
+				],
+				"tooltip": "This is a tooltip"
+			}]
+		};
 	});
