ab1596404edcf10b383a0234b168199b0495bb22
Robert Chang <cht8687@gmail.com>
2014-10-04 22:42:56 +1000

    feat(history): delete dummy data call card detail service

--- www/js/controllers/history.js
+++ www/js/controllers/history.js
@@ -1,36 +1,8 @@
 'use strict';
 angular.module('starter.controllers')
 	.controller('HistoryCtrl', function ($scope, card) {
-		/* Example how to show charts 
-		$scope.cards = card.all();
-		$scope.chartType = 'bar'
-		$scope.config1 = {
-			labels: false,
-			title: "Weekly",
-			legend: {
-				display: false,
-				position: 'left'
-			},
-			innerRadius: 0
-		};
-		$scope.data1 = {
-
-			data: [{
-				x: "M",
-				y: [2.75]
-			}, {
-				x: "T",
-				y: [3.5]
-			}, {
-				x: "W",
-				y: [2.75]
-			}, {
-				x: "T",
-				y: [6.8]
-			}, {
-				x: "F",
-				y: [5.5]
-			}]
-		};*/
-
+		card.getJsonCardDetailsArray(function (error, data) {
+			console.log(data);
+			$scope.cards = data;
+		});
 	});
