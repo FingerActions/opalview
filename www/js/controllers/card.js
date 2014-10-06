b752d06313c1e6d19f9e7beff153ab1bf93ff998
Robert Chang <cht8687@gmail.com>
2014-10-06 14:14:37 +1100

    beautify(atom): atom auto beautify this script

--- www/js/controllers/card.js
+++ www/js/controllers/card.js
@@ -1,73 +1,73 @@
 'use strict';
 angular.module('starter.controllers')
-	.controller('CardCtrl', function (account, $ionicPopup, $http, $scope, $ionicModal, card, $ionicLoading) {
-		card.getAll(function(error, data) {
-			$scope.cards = data;
-		});
-		$scope.login = function () {
-    	$ionicLoading.show({
-      	template: 'Loading...'
-    	});
-			account.login($scope.username, $scope.password, function (error) {
-    	$ionicLoading.hide();
-			if (error) {
-				$ionicPopup.alert({
-					title: 'Sorry',
-					template: error.message
-				});
-			} else {
-				//logged in
-				var cards = [];
-				card.getAll(function(error, data) {
-					cards = data;
-				});
-				var length = cards.length;
-				while(length-- > 0) {
-				   card.loadCardActivities(cards[length].activities,cards[length].cardNumber,0,8,2014);
-				}
-				$scope.cards = cards;
-				$scope.modal.hide();
-				$ionicPopup.alert({
-					title: 'Congratulations!',
-					template: 'You have added your opal cards.'
-				});
-			}
-		});
-		};
+  .controller('CardCtrl', function(account, $ionicPopup, $http, $scope, $ionicModal, card, $ionicLoading) {
+    card.getAll(function(error, data) {
+      $scope.cards = data;
+    });
+    $scope.login = function() {
+      $ionicLoading.show({
+        template: 'Loading...'
+      });
+      account.login($scope.username, $scope.password, function(error) {
+        $ionicLoading.hide();
+        if (error) {
+          $ionicPopup.alert({
+            title: 'Sorry',
+            template: error.message
+          });
+        } else {
+          //logged in
+          var cards = [];
+          card.getAll(function(error, data) {
+            cards = data;
+          });
+          var length = cards.length;
+          while (length-- > 0) {
+            card.loadCardActivities(cards[length].activities, cards[length].cardNumber, 0, 8, 2014);
+          }
+          $scope.cards = cards;
+          $scope.modal.hide();
+          $ionicPopup.alert({
+            title: 'Congratulations!',
+            template: 'You have added your opal cards.'
+          });
+        }
+      });
+    };
 
-		$scope.logout = function () {
-			account.logout(function (error) {
-				if (error) {
-					$ionicPopup.alert({
-						title: 'Sorry',
-						template: error.message
-					});
-				}
-			});
-		};
+    $scope.logout = function() {
+      account.logout(function(error) {
+        if (error) {
+          $ionicPopup.alert({
+            title: 'Sorry',
+            template: error.message
+          });
+        }
+      });
+    };
 
-		$ionicModal.fromTemplateUrl('templates/account/fing-acts-login.html', {
-			scope: $scope,
-			animation: 'slide-in-up'
-		}).then(function (modal) {
-			$scope.modal = modal;
-		});
-		$scope.openLoginModal = function () {
-			$scope.modal.show();
-		};
-		$scope.closeLoginModal = function () {
-			$scope.modal.hide();
-		};
-		//Cleanup the modal when we're done with it!
-		$scope.$on('$destroy', function () {
-			$scope.modal.remove();
-		});
-		// Execute action on hide modal
-		$scope.$on('modal.hidden', function () {
-			// Execute action
-		});
-		// Execute action on remove modal
-		$scope.$on('modal.removed', function () {
-			// Execute action
-		});
-	});
+    $ionicModal.fromTemplateUrl('templates/account/fing-acts-login.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+    $scope.openLoginModal = function() {
+      $scope.modal.show();
+    };
+    $scope.closeLoginModal = function() {
+      $scope.modal.hide();
+    };
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+      $scope.modal.remove();
+    });
+    // Execute action on hide modal
+    $scope.$on('modal.hidden', function() {
+      // Execute action
+    });
+    // Execute action on remove modal
+    $scope.$on('modal.removed', function() {
+      // Execute action
+    });
+  });
