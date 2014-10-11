58adba2c6f062a02debce0c92fad52d9c3182f10
Robert Chang <cht8687@gmail.com>
2014-10-11 15:12:46 +1100

    feat(home): add in refresher for home page

--- www/js/controllers/card.js
+++ www/js/controllers/card.js
@@ -1,15 +1,28 @@
 'use strict';
 angular.module('starter.controllers')
   .controller('CardCtrl', function(account, $ionicPopup, $http, $scope, $ionicModal, card, $ionicLoading) {
-		var isLoggedin = $scope.isLoggedin = !!account.isLoggedin();
+    var isLoggedin = $scope.isLoggedin = !!account.isLoggedin();
+    $scope.doRefresh = function() {
+      card.getAll(function(error, data) {
+        if (error) {
+          $ionicLoading.hide();
+          $ionicPopup.alert({
+            title: 'Sorry',
+            template: error.message
+          });
+        } else {
+          loadCardsActivities(data, true);
+        }
+      });
+    };
 
-    function loadCardsActivities(data) {
-			var cards = data;
-			var length = cards.length;
+    function loadCardsActivities(data, refresh) {
+      var cards = data;
+      var length = cards.length;
 
-			while(length-- > 0) {
+      while (length-- > 0) {
         cards[length].activities = [];
-				card.loadCardActivities(function(error, data, status, headers, config) {
+        card.loadCardActivities(function(error, data, status, headers, config) {
           var cardIndexIndex = config.url.indexOf('cardIndex');
           var index = cardIndexIndex + 10;
           var url = config.url;
@@ -17,7 +30,7 @@ angular.module('starter.controllers')
           var urlLength = url.length;
 
           var nextChar = url[index];
-          while (nextChar!== '&' && ++index < urlLength) {
+          while (nextChar !== '&' && ++index < urlLength) {
             cardIndex += nextChar;
             nextChar = url[index];
           }
@@ -27,10 +40,13 @@ angular.module('starter.controllers')
           } else {
             $scope.cards[cardIndex].activities = data;
           }
-				}, length, 1);
-			}
-			$scope.cards = cards;
-		}
+        }, length, 1);
+      }
+      $scope.cards = cards;
+      if (refresh) {
+        $scope.$broadcast('scroll.refreshComplete');
+      }
+    }
 
     if (isLoggedin) {
       card.getAll(function(error, data) {
@@ -41,7 +57,7 @@ angular.module('starter.controllers')
             template: error.message
           });
         } else {
-          loadCardsActivities(data);
+          loadCardsActivities(data, false);
         }
       });
     }
@@ -52,7 +68,7 @@ angular.module('starter.controllers')
       });
       account.login(function(error) {
         if (error) {
-					$ionicLoading.hide();
+          $ionicLoading.hide();
           $ionicPopup.alert({
             title: 'Sorry',
             template: error.message
@@ -61,22 +77,22 @@ angular.module('starter.controllers')
           //logged in
           isLoggedin = $scope.isLoggedin = true;
           card.getAll(function(error, data) {
-						loadCardsActivities(data);
-						$ionicLoading.hide();
-						$scope.loginModal.hide();
-						$ionicPopup.alert({
-							title: 'Congratulations!',
-							template: 'You have added your opal cards.'
-						});
+            loadCardsActivities(data, false);
+            $ionicLoading.hide();
+            $scope.loginModal.hide();
+            $ionicPopup.alert({
+              title: 'Congratulations!',
+              template: 'You have added your opal cards.'
+            });
           });
         }
       }, $scope.username, $scope.password);
     };
 
     $scope.logout = function() {
-			$ionicLoading.show({
-				template: 'Loading...'
-			});
+      $ionicLoading.show({
+        template: 'Loading...'
+      });
       account.logout(function(error) {
         if (error) {
           $ionicPopup.alert({
@@ -85,10 +101,10 @@ angular.module('starter.controllers')
           });
         } else {
           isLoggedin = $scope.isLoggedin = false;
-					$ionicLoading.hide();
-					$scope.logoutModal.hide();
-					$scope.cards = [];
-				}
+          $ionicLoading.hide();
+          $scope.logoutModal.hide();
+          $scope.cards = [];
+        }
       });
     };
 
@@ -105,29 +121,29 @@ angular.module('starter.controllers')
       $scope.loginModal.hide();
     };
 
-		$ionicModal.fromTemplateUrl('templates/account/fing-acts-logout.html', {
-			scope: $scope,
-			animation: 'slide-in-up'
-		}).then(function(modal) {
-			$scope.logoutModal = modal;
-		});
-		$scope.openLogoutModal = function() {
-			$scope.logoutModal.show();
-		};
-		$scope.closeLogoutModal = function() {
-			$scope.logoutModal.hide();
-		};
-		//Cleanup the modal when we're done with it!
-		$scope.$on('$destroy', function() {
-			$scope.loginModal.remove();
-			$scope.logoutModal.remove();
-		});
-		// Execute action on hide modal
-		$scope.$on('modal.hidden', function() {
-			// Execute action
-		});
-		// Execute action on remove modal
-		$scope.$on('modal.removed', function() {
-			// Execute action
-		});
+    $ionicModal.fromTemplateUrl('templates/account/fing-acts-logout.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.logoutModal = modal;
+    });
+    $scope.openLogoutModal = function() {
+      $scope.logoutModal.show();
+    };
+    $scope.closeLogoutModal = function() {
+      $scope.logoutModal.hide();
+    };
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+      $scope.loginModal.remove();
+      $scope.logoutModal.remove();
+    });
+    // Execute action on hide modal
+    $scope.$on('modal.hidden', function() {
+      // Execute action
+    });
+    // Execute action on remove modal
+    $scope.$on('modal.removed', function() {
+      // Execute action
+    });
   });
