f000a57dfc19a3900f7ba249f175f880dd4c3f3e
steve mao <steve.mao@healthinteract.com.au>
2014-10-13 10:04:10 +1100

    fix(history): use ng-class and ng-click properly

--- www/js/controllers/card-details.js
+++ www/js/controllers/card-details.js
@@ -1,14 +1,13 @@
 'use strict';
 angular.module('starter.controllers')
 	.controller('CardDetailsCtrl', function ($scope, $stateParams, card) {
-		card.getCard($stateParams.cardNumber, function(error, data){
+		card.getCard($stateParams.cardNumber, function(error, data) {
 			$scope.card = data;
 		});
 
 		$scope.type = 'Day';
-		$scope.setType = function(event) {
-			$scope.type = angular.element(event.target).text();
-			console.log($scope.type);
+
+		$scope.setType = function(type) {
+			$scope.type = type;
 		};
-		
 	});
