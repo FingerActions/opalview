bb8369d2aff9980c5f7cc5c5f9101f883efd4a46
Robert Chang <cht8687@gmail.com>
2014-10-19 01:33:33 +1100

    feat(today): add in refresher

--- www/js/controllers/today.js
+++ www/js/controllers/today.js
@@ -1,8 +1,21 @@
 'use strict';
 angular.module('fgts.controllers')
-	.controller('TodayCtrl', function ($scope, today) {
-	today.travelInfo(function(error, data){
-		$scope.serviceInfo = data;
-		console.log($scope.serviceInfo);
-	});
-});
+  .controller('TodayCtrl', function($scope, $ionicLoading, $ionicPopup, today) {
+
+    $scope.doRefresh = function() {
+
+      today.travelInfo(function(error, data) {
+        if (error) {
+          $ionicLoading.hide();
+          $ionicPopup.alert({
+            title: 'Sorry',
+            template: error.message
+          });
+        } else {
+          $scope.serviceInfo = data;
+          console.log($scope.serviceInfo);
+					$scope.$broadcast('scroll.refreshComplete');
+        }
+      });
+    };
+  });
