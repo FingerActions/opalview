2075c50f68767e34e8d23d49e84dc7fd84f55977
steve mao <steve.mao@healthinteract.com.au>
2014-10-18 18:36:12 +1100

    feat(history): chart

--- www/js/controllers/history-card.js
+++ www/js/controllers/history-card.js
@@ -1,6 +1,29 @@
 'use strict';
 angular.module('fgts.controllers')
   .controller('HistoryCardCtrl', function($scope, $stateParams, card) {
+    $scope.viewType = 'chart';
+
+    $scope.chartType = 'bar';
+
+    $scope.acConfig = {
+      labels: true,
+    };
+
+    $scope.acData = [];
+
+    $scope.toggleButtonImage = 'https://c2.staticflickr.com/4/3214/2699906907_a0d5efa66f.jpg';
+
+    $scope.toggleViewType = function() {
+      if ($scope.viewType === 'chart') {
+        $scope.viewType = 'text';
+        $scope.toggleButtonImage = 'http://pandas-xlsxwriter-charts.readthedocs.org/en/latest/_images/chart_grouped_column_farms.png';
+      }
+      else {
+        $scope.viewType = 'chart';
+        $scope.toggleButtonImage = 'https://c2.staticflickr.com/4/3214/2699906907_a0d5efa66f.jpg';
+      }
+    };
+
     card.getCardDetails($stateParams.cardNumber, function(error, data) {
       var opal = data;
       $scope.title = opal.cardNickName;
@@ -10,5 +33,14 @@ angular.module('fgts.controllers')
       $scope.setView = function(view) {
         $scope.viewBy = view;
       };
+
+      $scope.acData = {
+        series: ['1 Nov', '2 Nov', '3 Nov'],
+        data: [{
+          x: 'Days',
+          y: [8, 10, 10, 9, 10, 8, 10]
+        }]
+      };
+
     });
   });
