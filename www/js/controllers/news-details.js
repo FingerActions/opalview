80b0a9095797bd3dc64270d5f540ef1e61e14d0d
Robert Chang <cht8687@gmail.com>
2014-10-25 23:08:29 +1100

    fix(news details): improve the news details

--- www/js/controllers/news-details.js
+++ www/js/controllers/news-details.js
@@ -1,12 +1,17 @@
 'use strict';
 angular.module('fgts.controllers')
-  .controller('NewsDetailsCtrl', function($scope,$window) {
+  .controller('NewsDetailsCtrl', function($scope,$window,$sanitize) {
 
     var localTrackWorkContent = JSON.parse($window.localStorage.getItem('trackWorkContent'));
     var localTrackWorkLineName = $window.localStorage.getItem('lineName');
-
     $scope.lineName = localTrackWorkLineName;
     $scope.trackWorkContent = localTrackWorkContent;
-    console.log('moreInfoNote2: ' + localTrackWorkContent.lineDirection);
+    console.log('moreInfoNote2: ' + localTrackWorkContent.trackWorkItemHeading);
+    //trackWorkItemHeading
+    //trackWorkContent
+    //lineName
+
+    $scope.timeInfo = localTrackWorkContent.trackWorkItemHeading;
+    $scope.content =  localTrackWorkContent.trackWorkContentHolder;
 
   });
