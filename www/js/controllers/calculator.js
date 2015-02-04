d9de9153b994385abeddd9c27cce7aaa711857f3
Robert Chang <cht8687@gmail.com>
2015-02-05 02:29:20 +1100

    feat(train): now we can calculate train fares. To do: display fares in the next page.

--- www/js/controllers/calculator.js
+++ www/js/controllers/calculator.js
@@ -1,9 +1,12 @@
 'use strict';
 angular.module('fgts.controllers')
 	.controller('CalculatorCtrl', function ($scope, calculator,stations) {
-		calculator.train('Ashfield', 'Town Hall', function(error, data){
-			console.log(data);
-		});
+
+		//calculator.train('Ashfield', 'Town Hall', function(error, data){
+		//	console.log(data);
+		//});
+
+        var fromStation,toStation;
 
 		$scope.type = 'map';
 		$scope.setType = function(type) {
@@ -20,4 +23,26 @@ angular.module('fgts.controllers')
             $scope.stationFrom.push(stationsArray[length].name);
             $scope.stationTo.push(stationsArray[length].name);
         }
+
+        $scope.updateFrom = function(typed){
+
+            console.log(typed);
+            fromStation = typed;
+        }
+
+        $scope.updateTo = function(typed){
+            console.log(typed);
+            toStation = typed;
+        }
+
+        $scope.search = function(){
+
+            console.log("Calculate: " + fromStation + " to "+ toStation);
+            calculator.train(fromStation,toStation,function(error,data){
+
+                console.log(data);
+
+            });
+        };
+
 	});
